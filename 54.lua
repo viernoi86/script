cdd7510b4d06dba70f210c77c6ce8f6bfee13040
vienoi86 <wtomas2021486@gmail.com>
2026-09-19 19:59:41 +0200

    Load external Lua scripts for parkour ninja

---
+++ 54.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/viernoi86/script/refs/heads/main/be-a-parkour-ninja.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/viernoi86/script/refs/heads/main/2.lua"))()
+
